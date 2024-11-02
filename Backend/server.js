bed9e1ca40eac2556bbb5c49fea01eea99110023
Brayan Cifuentes <brayan.cifuentes@uptc.edu.co>
2024-11-02 17:55:49 -0500

    UPDATE CORRECT ORGANIZATION PROYECT

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require("./dataBase");
+const connection = require("../data_base/dataBase");
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+  
---
+++ data_base/dataBase.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2')
+
+
+const connection = mysql.createConnection({
+
+    host:'localhost',
+    user:'root',
+    password:'211100',
+    database:'clinicManager'
+});
+
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos MySQL');
+});
+
+module.exports = connection;
